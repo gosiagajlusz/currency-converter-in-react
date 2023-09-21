a8b2a1872c03c6e6443423e4d70bc4941dda174c
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-09-21 14:17:58 +0200

    css styles updated to grid version

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,37 +1,37 @@
 import "./style.css";
 
-const Form = ()=>{
-
-
-    return (
-<form className="form" action="https://postman-echo.com/get">
-        <fieldset className="form__fieldset">
-          <label>
-            <span className="form__labelText"> Kwota </span>
+const Form = () => {
+  return (
+    <form className="form" action="https://postman-echo.com/get">
+      <fieldset className="form__fieldset">
+        <div className="form__section">
+          <span className="form__labelText"> Kwota </span>
+          <div className="form__inputSelect">
             <input
+              className="form__money"
               name="money"
-              className="form__money js-form__money"
               type="number"
               step="0.01"
               min="1"
               required
             />
-          </label>
-          <label>
-            <select className="form__currencyType js-currency" name="currency">
-              <option value="euro">euro</option>
-              <option value="dolar">dolar</option>
-              <option value="frank szwajcarski">frank szwajcarski</option>
+            <select className="form__currencyType" name="currency">
+              <option value="EUR">EUR</option>
+              <option value="USD">USD</option>
+              <option value="CHF">CHF</option>
             </select>
-          </label>
+          </div>
+        </div>
+       
 
-          <p>
-            <button type="submit" className="form__button">
-              Przelicz!
-            </button>
-          </p>
-        </fieldset>
-      </form>)
-}
+        <div className="form__sectionForButton">
+          <button type="submit" className="form__button">
+            Przelicz!
+          </button>
+        </div>
+      </fieldset>
+    </form>
+  );
+};
 
 export default Form;
