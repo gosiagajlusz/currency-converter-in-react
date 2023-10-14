d8d5cb7722aa7121f683088aa2b540fe700de384
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-10-14 11:35:06 +0200

    clock styled in styled components way

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,15 +1,15 @@
-import "./style.css";
 import { useState, useEffect } from "react";
 import { useCurrentDate } from "./useCurrentDate";
 import { options } from "./options";
+import {DivClock} from "./styled"
 
 
 const Clock = () => {
   const date = useCurrentDate();
   return (
-    <div className="clock">
+    <DivClock>
       Dzisiaj jest {date.toLocaleString(undefined, options)}
-    </div>
+    </DivClock>
   );
 };
 
---
+++ src/Clock/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const DivClock = styled.div`
+  padding: 10px 10px 30px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  color: #686464;
+`;
