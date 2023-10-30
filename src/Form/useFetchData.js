69289b63700916ce38b3084df023fb87c1819fda
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-10-30 15:44:50 +0100

    simpilfied,and cleaned Form and useFetchData

--- src/Form/useFetchData.js
+++ src/Form/useFetchData.js
@@ -25,7 +25,6 @@ export const useFetchData = () => {
           data,
           meta,
         });
-        //tutaj chyba podawanie, że ma dac currency, a nie ogolnikowo,ze data spowodowalo błąd
       } catch (error) {
         setRatesData({
           status: "error",
@@ -39,39 +38,3 @@ export const useFetchData = () => {
 
   return { ratesData };
 };
-
-//zakomentowane do testu
-// const URL= "https://api.currencyapi.com/v3/latest?apikey=cur_live_NZ7GkaDNppJmHTw2AFyWLQO9jf0rOtdaJYc32yg2&currencies=EUR%2CUSD%2CCAD&base_currency=PLN"
-
-// export const useRatesData= ()=>{
-
-// const [ratesData, setRatesData] = useState({
-//   state: "Loading",
-// });
-
-// useEffect(() => {
-//   const fetchRates = async () => {
-//     try {
-//       const response = await fetch(URL
-//       );
-
-//       if (!response.ok) {
-//         throw new Error(response.statusText);
-//       }
-//       const { rates, date } = await response.json();
-//       setRatesData({
-//         state: "success",
-//         rates,
-//         date,
-//       });
-//     } catch {
-//       setRatesData({
-//         state: "Error",
-//       });
-//     }
-//   };
-
-//   setTimeout(fetchRates,1000);
-// },[]);
-// return ratesData;
-// };
